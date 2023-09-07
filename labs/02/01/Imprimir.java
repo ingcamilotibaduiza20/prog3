16e66a6689544c22cac9ebc610752aba7092e015
Camilo Tibaduiza <141367348+ingcamilotibaduiza20@users.noreply.github.com>
2023-09-07 13:46:40 -0500

    Add files via upload

---
+++ labs/02/01/Imprimir.java
@@ -0,0 +1,18 @@
+
+public class Imprimir {
+        
+    // Función para imprimir una matriz
+    public static void imprimirMatriz(double[][] matriz) {
+        int fila = matriz.length;
+        int columna = matriz[0].length;
+
+        System.out.println("Matriz resultante:");
+
+        for (int i = 0; i < fila; i++) {
+            for (int j = 0; j < columna; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
